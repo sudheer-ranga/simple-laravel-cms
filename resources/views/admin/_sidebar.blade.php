e8ffd9a0df34e75845bb02f8951bdab711973533
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-17 12:35:27 +0800

    Delete resources directory

--- resources/views/admin/_sidebar.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="list-group">
-    {!! link_to_route('admin.post.index', "Posts", [], ['class' => 'list-group-item']) !!}
-    {!! link_to_route('admin.post.trash.index', "Trashed Posts", [], ['class' => 'list-group-item']) !!}
-    {!! link_to_route('admin.tag.index', "Tags", [], ['class' => 'list-group-item']) !!}
-    {!! link_to_route('logout', "Logout", [], ['class' => 'list-group-item']) !!}
-</div>
--- resources/views/admin/post/_post_list.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="col-sm-12">
-    <h4>
-        {!! link_to_route('admin.post.show', $post->title, [$post->slug]) !!}
-    </h4>
-    <p>{!! $post->description !!}</p>
-</div><!-- /.col -->
